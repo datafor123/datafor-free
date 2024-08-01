feecd6796b4cb414bb6f87cba89b0b986b206084
GeCherry <shareluck_gcy@163.com>
2024-08-01 16:31:34 +0800

    default db use postgresql

--- assemblies/pentaho-data/src/main/resources/data/mysql/create_jcr_mysql.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE DATABASE IF NOT EXISTS `jackrabbit` DEFAULT CHARACTER SET latin1;
-
-CREATE USER 'jcr_user'@'localhost' identified by 'password';
-GRANT ALL PRIVILEGES ON jackrabbit.* TO 'jcr_user'@'localhost' WITH GRANT OPTION;
-
-commit;
--- assemblies/pentaho-data/src/main/resources/data/mysql/create_repository_mysql.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `hibernate` DEFAULT CHARACTER SET latin1;
-
-USE hibernate;
-
-CREATE USER 'hibuser'@'localhost' identified by 'password';
-GRANT ALL PRIVILEGES ON hibernate.* TO 'hibuser'@'localhost' WITH GRANT OPTION;
-
-commit;
